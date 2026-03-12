589842167379a77684975cd9447aa71a1a831049
roxcepret20-oss <roxcepret20@gmail.com>
2026-03-12 19:07:21 +0700

    footer, metadata,

---
+++ src/app/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "16px", opacity: 0.5, fontSize: "0.85rem" }}>
+      © 2026 cepret. All rights reserved
+    </footer>
+  );
+}
